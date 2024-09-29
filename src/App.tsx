b17d718b77244df4077059ef318c5166b7ef4b78
Josh-Harrison1600 <joshharrison2283@gmail.com>
2024-09-29 19:18:13 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-black flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-white text-5xl md:text-6xl font-semibold">
+          Hello, I'm <span className="text-red-500">Josh</span>.
+        </h1>
+        <h2 className="text-white text-3xl md:text-4xl mt-4">
+          I'm a Computer Science Student.
+        </h2>
+        <button className="mt-8 px-6 py-3 border-2 border-red-500 text-red-500 text-xl hover:bg-red-500 hover:text-white transition duration-300">
+          View my work ↓
+        </button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Adding Poppins font
+      },
+    },
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  plugins: [],
+}
