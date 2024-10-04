5980ce3baf22618243fba4d9c57a7b1982cb0011
Josh-Harrison1600 <joshharrison2283@gmail.com>
2024-10-03 21:38:50 -0300

    added gradient effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bgIMG from "./components/images/bg1.png";
 
 function App() {
+
+  // Create state to hold the cursor position
+  const [mousePos, setMousePos] = useState({ x: -175, y: -175 });
+
+  // Add event listener to track the mouse movement
+  useEffect(() => {
+    const handleMouseMovement = (event: MouseEvent) => {
+      setMousePos({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    // Attach event listener when the component is mounted
+    window.addEventListener('mousemove', handleMouseMovement);
+
+    // Cleanup event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMovement);
+    };
+  }, []);
+
+  // Create the illuminated background based on cursor position
+  const backgroundStyle = {
+    backgroundImage: `radial-gradient(circle at ${mousePos.x}px ${mousePos.y}px, rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 1) 250px), url(${bgIMG})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'black',
+  };
+
   return (
-    <div className="h-screen bg-black flex items-center justify-center">
+    <div className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={backgroundStyle}
+    >
       <div className="text-center">
         <h1 className="text-white text-5xl md:text-6xl font-semibold">
           Hello, I'm <span className="text-red-500">Josh</span>.
