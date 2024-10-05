86e1beb4d8ddd721ed551e4d602c90ae06e68700
Josh-Harrison1600 <joshharrison2283@gmail.com>
2024-10-04 21:36:42 -0300

    updated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import bgIMG from "./components/images/bg1.png";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function App() {
 
@@ -101,8 +102,20 @@ function App() {
     {/* NavBar */}
     {showNavbar && (
       <nav className='fixed top-0 left-0 w-full bg-black text-white py-4'>
-        <div className='container mx-auto px-4'>
-          <h1 className='text-2xl font-bold'>navbar</h1>
+        <div className='container mx-auto px-4 flex justify-between items-center'>
+          <h1 className='text-2xl font-bold text-red-500'><span className="inline-block hover:text-green-600 duration-300 transition-transform transform hover:scale-110">Josh's</span> <span className="inline-block hover:text-blue-600 duration-300 transition-transform transform hover:scale-110">Website</span></h1>
+            <div className='flex space-x-8 items-center'>
+              <a href="#home" className="font-semibold text-white hover:text-red-500 transition duration-300">Home</a>
+              <a href="#about" className="font-semibold text-white hover:text-red-500 transition duration-300">About</a>
+              <a href="#projects" className="font-semibold text-white hover:text-red-500 transition duration-300">Projects</a>
+              <a href="#contact" className="font-semibold text-white hover:text-red-500 transition duration-300">Contact</a>
+              <a href="https://github.com/Josh-Harrison1600" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500 transition duration-300">
+                <FaGithub size={24} />
+              </a>
+              <a href="https://www.linkedin.com/in/josh-harrison2283/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500 transition duration-300">
+                <FaLinkedin size={24} />
+              </a>
+            </div>
         </div>
       </nav>
     )}
