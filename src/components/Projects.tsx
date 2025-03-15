cbd1d5b056767d7dc67cd99946a5b0ff9dd08045
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2025-03-15 19:58:41 -0300

    added project videos, fixed links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { env } from 'process';
 import React, { forwardRef } from 'react';
 
 interface ProjectsProps {
@@ -6,22 +7,19 @@ interface ProjectsProps {
 
 const Projects = forwardRef<HTMLDivElement, ProjectsProps>(({ projectsText }, ref) => {
   return (
-    <div ref={ref} className='min-h-screen bg-gray-950 text-white p-8 pt-16'>
+    <div ref={ref} className='min-h-screen bg-gray-950 text-white p-8 pt-16 overflow-hidden'>
       <div data-aos="fade-up">
         <h2 className="text-4xl text-center mb-8">{projectsText}</h2>
 
         {/* Tarkov Randomizer */}
         <div className="flex flex-col md:flex-row items-center justify-between mb-16">
           <div className="md:w-[43%] mb-4 md:mb-0 text-center">
-            <a href="https://tarkov-randomizer.vercel.app/" target="_blank">
+            <a href="https://tarkov-randomizer-new.vercel.app/" target="_blank">
               <h3 className="inline-block text-2xl font-bold hover:text-blue-500 transition-all duration-300 transform hover:scale-110 text-center mb-4">Tarkov Randomizer</h3>
             </a>
             <p className="text-xl">
               Tarkov Randomizer is a work in progress web app that scrapes all of the gear from the Tarkov Wiki using <a href="https://www.npmjs.com/package/cheerio" target="_blank" className='inline-block hover:text-blue-500 underline transition-all duration-300 transform hover:scale-105'>Cheerio </a>, stores it in an AWS MySQL database and then uses a REST API to send it to the frontend into a customizable randomizer so you can get a random loadout to play with in raid.
               The scraper it set to run automatically once a month using <a href="https://github.com/features/actions" target="_blank" className='inline-block hover:text-blue-500 underline transition-all duration-300 transform hover:scale-105'>GitHub Actions</a> to account for any content updates in the game!
-            </p>
-            <p className="text-xl mt-2">
-              <span className='font-bold text-red-500'>Note:</span> I am currently reworking this project for a nicer frontend, <span className='font-bold text-red-500'>this is not final!</span>
             </p>
             <p className="text-xl mb-4 mt-4">Tarkov Randomizer is being made using:</p>
             <ul className='list-disc list-inside text-lg ml-4'>
@@ -68,10 +66,14 @@ const Projects = forwardRef<HTMLDivElement, ProjectsProps>(({ projectsText }, re
 
           </div>
           <div data-aos="flip-down" className="md:w-[55%] hover:scale-105 duration-300">
-            <a href="https://tarkov-randomizer.vercel.app/" target="_blank" className='block'>
-              <img src={require('./images/tarkovRandomizer.png')} className="w-full border-2 border-transparent hover:border-blue-500 hover:scale-105 transition-all duration-300" />
+            <a href="https://tarkov-randomizer-new.vercel.app/" target="_blank" className='block'>
+              <video autoPlay loop muted
+              className="w-full border-2 border-transparent hover:border-blue-500 hover:scale-105 transition-all duration-300"
+              >
+                <source src={process.env.REACT_APP_TARKOV_VIDEO} type="video/mp4"/>
+                Your Browser does not support this video
+              </video>
             </a>
-            
           </div>
         </div>
 
@@ -85,14 +87,14 @@ const Projects = forwardRef<HTMLDivElement, ProjectsProps>(({ projectsText }, re
             Kana Typer is a web app that helps users learn the main two Japanaese phonetic systems, Hiragana & Katakana. The user can select individual symbols, specific categories, or the entire system to study and review! 
               <p className="text-xl mb-4 mt-4">Kana Typer was made using:</p>
             <ul className='list-disc list-inside text-lg ml-4'>
-              <li>
-                <a href="https://www.typescriptlang.org/" target="_blank" className='inline-block hover:text-red-500 transition-all duration-300 transform hover:scale-105 mb-2'>
-                  TypeScript
+            <li>
+                <a href="https://react.dev/" target="_blank" className='inline-block hover:text-red-500 transition-all duration-300 transform hover:scale-105 mb-2'>
+                  React
                 </a>
               </li>
               <li>
-                <a href="https://react.dev/" target="_blank" className='inline-block hover:text-red-500 transition-all duration-300 transform hover:scale-105 mb-2'>
-                  React
+                <a href="https://www.typescriptlang.org/" target="_blank" className='inline-block hover:text-red-500 transition-all duration-300 transform hover:scale-105 mb-2'>
+                  TypeScript
                 </a>
               </li>
               <li>
@@ -106,11 +108,12 @@ const Projects = forwardRef<HTMLDivElement, ProjectsProps>(({ projectsText }, re
           </div>
           <div data-aos="flip-down" className="md:w-[55%] hover:scale-105 duration-300">
             <a href="https://kana-typer-fawn.vercel.app/" target="_blank" className="block">
-              <img
-                src={require('./images/kanaTyper.png')}
-                alt="Project Two"
+              <video autoPlay loop muted
                 className="w-full border-2 border-transparent hover:border-red-500 hover:scale-105 transition-all duration-300"
-              />
+              >
+                <source src={process.env.REACT_APP_KANATYPER_VIDEO} type="video/mp4"/>
+                Your Browser does not support this video
+              </video>
             </a>
           </div>
         </div>
@@ -136,6 +139,11 @@ const Projects = forwardRef<HTMLDivElement, ProjectsProps>(({ projectsText }, re
               <li>
                 <a href="https://tailwindcss.com/" target="_blank" className='inline-block hover:text-blue-600 transition-all duration-300 transform hover:scale-105 mb-2'>
                   Tailwind
+                </a>
+              </li>
+              <li>
+                <a href="https://aws.amazon.com/s3/" target="_blank" className='inline-block hover:text-blue-600 transition-all duration-300 transform hover:scale-105 mb-2'>
+                  AWS S3
                 </a>
               </li>
             </ul>
